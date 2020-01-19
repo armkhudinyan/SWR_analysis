4cc1bb9d2dacf84c80376782ddaef72e684819aa
armkhudinyan <47722475+armkhudinyan@users.noreply.github.com>
2020-01-19 11:41:08 +0000

    Update Regression_analysis.R

--- regression/Regression_analysis.R
+++ regression/Regression_analysis.R
@@ -1,4 +1,5 @@
-setwd('D:/Thesis Practical/R/Scripts')
+# setup the working directory
+#setwd('D:/Thesis Practical/R/Scripts')
 library(keras)
 source("Explorativeanalysis.R")
 library(onehot)
@@ -14,7 +15,6 @@ rmse <- function(error)
 {
   sqrt(mean(error^2))
 }
-
 
 # read the data
 data<- read.csv(file = "lriv_verjnakan.csv", header=T, sep = ",")# T is for true
@@ -59,15 +59,16 @@ data2 <- data.new[,-c(1)]
 #scaled.data <- (data2 - min(data2)) / (max(data2)-min(data2))
 x <- data.frame(data2)
 
-selected <- x[,c(1,11,12,15,16,17,18,19,20,21,22,23,24,25)]#
-selected <- x[,c(1,4,5,7,9,13,14,15,16,17,18)]# 
-selected <- x[,c(1,5,7,8,10,13,14,17,26)]# another good->SAR+ancill
-selected <- x[,c(1,5,7,8,10,13,14,16,17,26)]# too good-> full
+selected <- x[,c(1,11,12,15,16,17,18,19,20,21,22,23,24,25)]
+selected <- x[,c(1,4,5,7,9,13,14,15,16,17,18)] 
+selected <- x[,c(1,5,7,8,10,13,14,17,26)] # another good->SAR+ancill
+selected <- x[,c(1,5,7,8,10,13,14,16,17,26)] # best selection-> full data combination
 #y <- data[,c(2,3,4,5,6,7,8,9,10,11,12,13,14)]
 #y <- x[,c(1,16,17,18,19,20,21,22,23)]
 #ggpairs(y)
 #selected <- x[,c(1,6,7,9,11,16,17,19)]
 #write.csv(scaled_data, file='scaled2')
+
 #============================
 #Explorative analysis
 #============================
@@ -75,7 +76,7 @@ selected <- x[,c(1,5,7,8,10,13,14,16,17,26)]# too good-> full
 #pairs(x[,c(15,16)], lower.panel = panel.smooth, upper.panel = panel.cor)
 
 #============================
-#Modellling
+#Modelling
 #============================
 require(MASS)
 mod1lr = lm(AGB_pixel ~ .,data = selected)
@@ -99,9 +100,10 @@ RMSE <- rmse(error)
 RMSE
 error
 plot(mod2lr, 1)
-#===================================== MODEL DIAGNOSTICS========================
+#===================================== 
+#MODEL DIAGNOSTICS
+#=====================================
 #Evaluation asumptions about residuals
-#============================++=======
 #normality distribution in residuasl
 library(car)
 library(ggpubr)
@@ -114,9 +116,10 @@ shapiro.test(mod2lr$residuals)
 # From the output, the p-value > 0.05 implying that the distribution
 # of the data are not significantly different from normal distribution. 
 # In other words, we can assume the normality.
-#=========================================================================
+#=====================================
 #another test is called kolmogorov-smirnov test...
 #Homocedasticity (variance constant)
+#=====================================
 hist(mod2lr$residuals)
 
 #Autocorrelation TEST CORRELOGRAM
@@ -128,11 +131,13 @@ mod3 = lm(error[-n] ~ error[-1])
 summary(mod3)
  
 
-#================Cross_Validation================
+#===============================================
+# Cross_Validation
+#===============================================
 library(caret)
 
 #LOO cross-validation
-# Define training control
+# Define training control -> Leave One Out 
 train.control <- trainControl(method = "LOOCV")
 # Train the model
 model <- train(AGB_pixel ~., data = selected, method = "lm",
@@ -163,9 +168,9 @@ model <- train(AGB_pixel ~., data = selected, method = "lm",
 # Summarize the results
 print(model)
 
-
-
-#print CSV file=================================================
+#================================================
+# Write CSV file
+#================================================
 out <- matrix(nrow = nrow(x), ncol = 4)
 out[ , 1] <- data$plot_numb
 out[ , 2] <- data$AGB_pixel
@@ -174,7 +179,6 @@ out[ , 4] <- Y
 out <- data.frame(out)
 colnames(out) <- c('plot_nom', 'Reference_AGB','Prediction error', "Predicted_AGB")
 #write.csv(out, "Model 1 predicted.csv")#olivedrab
-
 
 # plot dependant variable against residuals
 pda <- ggplot(out, aes(x = Reference_AGB, y = Predicted_AGB)) +
