3f2cf7600bb4469f24e610d23fb4c2fc3e0a99f1
armkhudinyan <armkhudinyan@gmail.com>
2020-03-02 15:15:26 +0000

    Added AL real experiment code

---
+++ dgt_data/scripts/data_subsetting.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 15:06:33 2020
+
+@author: arman
+"""
+
+import time
+import os
+from os.path import join, dirname
+
+import numpy as np
+import pandas as pd
+
+import rasterio
+from rasterio.plot import show
+from rasterio import features
+from rasterio import Affine 
+import shapely
+import geopandas as gpd
+import gdal
+import ogr
+from shapely.geometry import Point
+from sklearn.impute import SimpleImputer 
+
+import glob
+from collections import Counter
+import pickle
+#===================
+# Defining paths
+#===================
+#sys.path.append(join(dirname(__file__), '..', '..'))
+PATH = r'C:\Users\arman\Desktop\ActiveLearning\Experiment\dgt_joao'
+
+all_train_path = join(PATH, 'train_data', 'sampled_point_coordinates_s2.csv')
+xy_29SND_path = join(PATH, 'train_data', 'sample_xy_29SND.csv')
+out_path = join(PATH, 'train_data')
+
+#==============================================================================
+# Define functions for saving and loading pickles
+
+def pickle_save(fname, data):
+  filehandler = open(fname,"wb")
+  pickle.dump(data,filehandler)
+  filehandler.close() 
+  print('saved', fname, os.getcwd(), os.listdir())
+
+def pickle_load(fname):
+  #print(os.getcwd(), os.listdir())
+  file = open(fname,'rb')
+  data = pickle.load(file)
+  file.close()
+  #print(data)
+  return data
+
+#==============================================================================
+# fixt the start time
+t0 = time.time()
+# fixing the random seed for reproducibility
+seed=np.random.seed(7)
+
+#==============================
+# load csv files
+#==============================
+# make a list with all raster's paths
+all_train = pd.read_csv(all_train_path, index_col=0)
+print('Classes', class_names = all_train.class_decoded.unique().tolist())
+xy_29SND = pd.read_csv(xy_29SND_path)
+
+xy_29SND = xy_29SND.drop(columns =['class', 'y_geo', 'x_geo', 'class_deco'])
+#xy_29SND = xy_29SND.loc[:,'ObjectedID'].copy()
+
+sampled_29SND = xy_29SND.set_index('ObjectedID')\
+                .join(all_train.set_index('ObjectedID'), lsuffix='_l', rsuffix='_r', how = 'left')\
+                .rename(columns={'class':'classes'})\
+                .reset_index()
+                  
+# save to csv
+sampled_29SND.to_csv(join(out_path,"sampled_29SND.csv"), sep=',',header=True, index=True)
+
+# subsetting the training data
+class_id = sampled_29SND.classes.unique()
+class_size = sampled_29SND.groupby('classes').size()
+class_size_dict = dict(class_size)
+
+to_list = [class_size_dict[keys] for keys in class_size_dict]  
+
+# create list containing sample size per class
+sample_size = []
+
+for i in to_list:
+    if i >250:
+        sample_size.append(250)
+    else:
+        sample_size.append(i)
+
+df_sample_size = pd.Series(sample_size)
+
+# sample data by given sample size
+train_29SND = sampled_29SND.groupby('classes')\
+               .apply(lambda x: x.sample(df_sample_size[x['classes'].iloc[0]]))\
+               .reset_index(drop=True)
+
+# save training samples
+train_29SND.to_csv(join(out_path,"train_29SND.csv"), sep=',',header=True, index=True)
+
+
+#==============================
+# Select The most important features for classification
+#==============================
+
+feat_ranking = pd.read_csv(join(PATH, 'feature_importance', 'feature_rankings.csv'))
+train_29SND = pd.read_csv(join(out_path, 'train_29SND.csv'))
+
+# list of best 40 featurs names
+best_feat = feat_ranking['feature'].tolist()[:40]
+train_29SND_best_feat = train_29SND.loc[:, ['classes']+best_feat]
+# save training data with best features
+train_29SND_best_feat.to_csv(join(out_path, 'train_best_40.csv'))
+
+
+# recrd the experiment time
+t1 = time.time()
+run_time = round((t1-t0)/60, 2)
+
+experiment_time = {}
+
+experiment_time['run_time']= []
+experiment_time['run_time'].append(run_time)
+
+# save pickled disctionary with results
+name = 'run_time_sampling'
+#pickle_save(name, experiment_time)
+
+
+print('Training time:', round((t1-t0)/60,2), 'mins')
--- dgt_data/scripts/image_classif.py
+++ dgt_data/scripts/image_classif.py
@@ -17,8 +17,7 @@ import rasterio
 from rasterio.plot import show
 from rasterio import features
 from rasterio import Affine 
-import shapely
-import geopandas as gpd
+#import geopandas as gpd
 import gdal
 import ogr
 from shapely.geometry import Point
@@ -32,26 +31,17 @@ import pickle
 # Defining paths
 #===================
 #sys.path.append(join(dirname(__file__), '..', '..'))
-PATH = r'C:\Users\arman\Desktop\ActiveLearning\Experiment\my_data'
-SENTINEL_PATH = r'G:\DGT_data'
+PATH = r'C:\Users\arman\Desktop\ActiveLearning\Experiment\dgt_joao'
 
+SENTINEL_PATH = r'\\dgt-759\S2_2018\Theia_S2process\T29SND\composites'
+METRICS_PATH = join(SENTINEL_PATH, 'metrics')
+INDICES_PATH = join(SENTINEL_PATH, 'indices')
+#INDICES_METRICS_PATH = join(SENTINEL_PATH, 'indices', 'metrics')
 
-s1_path = join(PATH, 'sentinel1')
-s2_path = join(PATH, 'sentinel2')
-buffered_cos_path = join(PATH, 'data')
-shape_path = join(PATH,  'cos_shape', 'COS_2015_clipped.shp')
-csv_path = join(PATH, 'data','train_sampled.csv')
+TRAIN_PATH = join(PATH, 'train_data','train_best_40.csv')
+FEATURE_NAME_PATH = join(PATH, 'feature_importance', 'feature_rankings.csv')
 
-out_path = join(PATH, 'data')
-
-# raster band's names
-BANDS_NAME = {
-            'S2_N1' :  ['B2',  'B3',  'B4',   'B5',   'B8'  ],
-            'S2_N2' :  ['B8A', 'B11', 'NDVI', 'NDMI', 'NDBI'],
-            'S1'    :  ['fallVV', 'winterVV', 'springVV', 'summerVV', 
-                                  'fallVH', 'winterVH', 'springVH', 'summerVH'],
-            'S1_GLCM': ['savg', 'ent', 'corr', 'idm', 'var', 'diss', 'contrast', 'asm']
-            }
+OUT_PATH = join(PATH, 'results')
 
 #==============================================================================
 # Define functions for saving and loading pickles
@@ -71,10 +61,10 @@ def pickle_load(fname):
   return data
 
 #=======================CLASSIFICATION MODELS==================================
-def RF(x_train, y_train, image_array):
+def RF(X_train, y_train, image_array):
     '''Random Forest Classifier'''
     classifier = RandomForestClassifier(n_estimators=500, n_jobs=-1)
-    classifier.fit(x_train, y_train)
+    classifier.fit(X_train, y_train)
     y_probab = classifier.predict_proba(image_array)
     predicted = classifier.predict(image_array)
     return (y_probab, predicted)
@@ -96,47 +86,80 @@ t0 = time.time()
 seed=np.random.seed(7)
 
 #==============================
-# load raster files
+# Define the proper paths for rasters
 #==============================
-# make a list with all raster's paths
-rasters = []
+# make a list containg the best ranked features
+feat_ranking = pd.read_csv(FEATURE_NAME_PATH)
+best_feat = feat_ranking['feature'].tolist()[:40]
+# change name from Portugal to the cpecified tile
+best_feat = [path.replace('Portugal','T29SND') for path in best_feat]
+
+# categorize the rasters
+best_bands   = [band_name for band_name in best_feat if "." in band_name]
+best_bands.sort()
+best_metrics = [metric_name for metric_name in best_feat if 'q' in metric_name]
+best_indices = [index_name for index_name in best_feat if '_N' in index_name]
+
+# make a list with combined index and metrics raster's paths
+rasters_path = []
 
 search_criteria = "*.tif"
-for path in (s2_path, s1_path):
+for path in ( INDICES_PATH, METRICS_PATH):
     querie = join(path, search_criteria)   
     raster_path = glob.glob(querie)
-    rasters.extend(raster_path)
+    #print(raster_path)
+    rasters_path.extend(raster_path)
+
 # choose subset of the rasters
-rasters =  [rasters[index] for index in [1]]
+matchers = best_metrics + best_indices
+metric_index = [path for path in rasters_path if any(i in path for i in matchers)]
+best_ind_metr_path = [path for path in metric_index if 'b3_q90-' not in path] # drop extra selected band
 
-# Read the label and rs imagery rasters as array
+#==============================================================================
+'''
+# define bands path
+bands_path = []
+
+search_criteria = "*.tif"
+for path in (SENTINEL_PATH,):
+    querie = join(path, search_criteria)   
+    raster_path = glob.glob(querie)
+    bands_path.extend(raster_path)
+''' 
+
+#==============================
+# load raster files
+#==============================
+# Read the rasters as arraycand append to list
 bands_list = []
+bands_names = []
 
-labels = rasterio.open(join(buffered_cos_path, 'buffer_20_cos2015.tif'))
-bands_list.append(labels.read(1))
-
-# read rasters as array, fill missing values and appent to the list
-imputer = SimpleImputer(missing_values = np.nan, strategy ="mean")
-
-for raster in rasters:
+# load indices and metrics arrays
+for raster in best_ind_metr_path[:2]:
     src = rasterio.open(raster)
     out_meta = src.meta.copy()
     proj = src.crs
     transform = src.get_transform()
-    count = src.count 
-    #for b in range(1, count + 1):
-    for b in [1,2]:
-        # read a single band as 2D array
-        band = src.read(b)
-        rows, cols = band.shape
-        # imputing missing data 
-        imputer = imputer.fit(band) 
-        band = imputer.transform(band) 
+    rows, cols = src.shape
+    band_name = raster.split('\\')[-1].split('.')[0]
+    band = src.read(1)
+    bands_list.append(band)
+    bands_names.append(band_name)
+    del band
 
-        bands_list.append(band)
-        del band
-        
+# load bends arrays
+for band_name in best_bands[:2]:
+    search = f'{band_name.split(".")[0]}.tif'
+    band_n = int(band_name.split(".")[1])
+    ras_path = join(SENTINEL_PATH, search)
+    #ras_path = glob.glob(query)
+    src = rasterio.open(ras_path)
+    band = src.read(band_n)
+    bands_list.append(band)
+    bands_names.append(band_name)
+    del band
 
+'''
 # create coordinates for the pixels
 x = np.arange(0, cols, 1)
 y = np.arange(0, rows, 1)
@@ -144,46 +167,51 @@ xx,yy = np.meshgrid(x,y,)
 
 bands_list.append(xx)
 bands_list.append(yy)
-
+'''
 # make a d-stack array from the bands (3D)
 data_stack = np.dstack(bands_list)
 
 _, _, n_bands = data_stack.shape
 # number of pixels on one band
-n_pixels = rows*cols
+n_pixels = rows*(cols)
 # stack bands as columns each (2D)
 flat_pixels = data_stack.reshape((n_pixels, n_bands))
-del data_stack
+# fill missing values 
+df_stack = pd.DataFrame(flat_pixels, columns=bands_names )
+
+#df_stack = pd.DataFrame(flat_pixels).fillna(method='bfill', axis=0)
+#flat_pixels = df_stack.values
+#del data_stack
 
 # drop Nones using the rasterised COS
-index = np.argwhere(flat_pixels[:,0]==-1)
-all_stack = np.delete(flat_pixels, index, axis = 0)
-del flat_pixels
+#index = np.argwhere(flat_pixels[:,0]==-1)
+#all_stack = np.delete(flat_pixels, index, axis = 0)
+#xy = all_stack[:, [-2,-1]]
+#del flat_pixels
 
 #==============================
 # Loading the training data
 #==============================
-df = pd.read_csv(csv_path)
-df = df.drop('Unnamed: 0', axis=1)
+train_data = pd.read_csv(TRAIN_PATH, index_col=0)
 
 # filling the missing values
-imputer = SimpleImputer(missing_values = np.nan, strategy ="mean") 
+#imputer = SimpleImputer(missing_values = np.nan, strategy ="mean") 
 
-x_train = df.iloc[:, 6:8].values
-imputer1 = imputer.fit(x_train) 
-x_train = imputer1.transform(x_train) 
+X_train = train_data.iloc[:, 1:].values
+#imputer1 = imputer.fit(x_train) 
+#x_train = imputer1.transform(x_train) 
 
-y_train = df.iloc[:, 0].values
+y_train = train_data.iloc[:, 0].values
 
-image_array = all_stack[:, 1:3] # unmute in case of xy coordinets
-#imputer2 = imputer.fit(image_array) 
-#image_array = imputer2.transform(image_array) 
 
+# organize classification data
+col_order = train_data.columns[1:]
+image_array = df_stack.loc[:, col_order].values
 
 #==============================
 # Train RF model and classify image
 #==============================
-y_probab, predicted = RF(x_train, y_train, image_array)
+y_probab, predicted = RF(X_train, y_train, image_array)
 
 # Mapping the uncertainty
 uncertainty = MarginSamplingSelection(y_probab)
@@ -191,34 +219,74 @@ uncertainty = MarginSamplingSelection(y_probab)
 #==============================
 # Save classification and uncertainty maps
 #==============================
-columns = ['classes'] + BANDS_NAME['S2_N2'][0:2] + ['x','y']
-     
-# Make a DataFrame from the long_stacked images
-df_coords = pd.DataFrame(all_stack, columns=columns)
 
-#==============================
-# Pass the real CRS to X and Y
-#==============================
-# set the transformation factor as Affine
-transf = src.meta['transform']
+'''
+# make dataframe from arrays containing raster x and y 
+columns1=['pred','x','y']
+columns2=['uncert','x','y']
 
-# select the centeroid of the pixel.
-# for this we reduce the original pixel coordinates by
-# the half of the pixel size (5 m in this case)
-transf = Affine(10.0, 0.0, 499980.0,
-               0.0, -10.0, 4400050.0)
+# define empty DataFrame with true raster size
+df_origin_pred = pd.DataFrame(predicted, columns=columns1)
+df_origin_pred.pred = np.nan
+df_origin_pred = df_origin_pred.set_index(['x','y'])
 
-geo_coords = df_coords.apply(
-    lambda row: transf*(row['x'], row['y']), axis=1
-)
+df_origin_uncert = pd.DataFrame(uncertainty, columns=columns2)
+df_origin_uncert.pred = np.nan
+df_origin_uncert = df_origin_uncert.set_index(['x','y'])
+#df_origin2 = df_origin.rename(columns = {'pred':'uncert'}, inplace = True)
 
 
-df_coords['x_geo'] = geo_coords.apply(lambda x: x[0])
-df_coords['y_geo'] = geo_coords.apply(lambda x: x[1])
+# recover raster for prediction
+#predicted = all_stack[:,0]
+pred_stack = np.hstack([np.expand_dims(predicted, -1), xy])
+df_pred = pd.DataFrame(pred_stack,columns=columns1) # shorter list
+df_pred = df_pred.set_index(['x','y'])
+df_pred = df_origin_pred.join(df_pred, lsuffix = '_old', rsuffix = '_new', how='outer')
 
-# drop pixel coords for raster
-df_coords = df_coords.drop(columns=['x','y'])
+# recover raster for uncertainty
+uncert_stack =  np.hstack([np.expand_dims(uncertainty, -1), xy])
+df_uncert = pd.DataFrame(uncert_stack, columns=columns2) # shorter list
+df_uncert = df_uncert.set_index(['x','y'])
+df_uncert = df_origin_uncert.join(df_uncert, lsuffix = '_old', rsuffix = '_new', how='outer')
 
+# reshape the vector raster to its original state to form the raster
+classif_map = np.reshape(df_pred.loc['pred_new'].values, (rows, cols))
+uncert_map  = np.reshape(df_uncert.loc['uncert_new'].values, (rows, cols))
+'''
+
+# in case there is no missing values and output array is equal to raster array
+classif_map = np.reshape(predicted.astype('float32'), (rows, cols))
+uncert_map  = np.reshape(uncertainty.astype('float32'), (rows, cols))
+
+#=====================
+# Write tif files
+#=====================
+#rasterize using the shape and transform of the satellite image
+out_meta  = src.meta.copy()
+
+out_meta.update({
+               "driver": "GTiff",
+               "dtype" : 'float32',
+               "nodata": None, #np.nan,
+               "height": src.height,
+               "width" : src.width,
+               "transform": src.transform,
+               "count" : 1,
+               #to any crs by defining here. if you don't do so, 
+        		   # it sets up to wgs84 geographic by default (weird tho)
+               #"crs": "+proj=utm +zone=29 +ellps=WGS84 +datum=WGS84 +units=m +no_defs "
+               "crs":  out_meta['crs']
+              })
+          
+# Write the raster to disk
+with rasterio.open(join(OUT_PATH, 'lulc_0.tif'), "w", **out_meta) as dest:
+    dest.write(classif_map, indexes=1)  
+
+with rasterio.open(join(OUT_PATH, 'uncertainty_0.tif'), "w", **out_meta) as dest:
+    dest.write(uncert_map, indexes=1)  
+
+
+'''
 # Output the simulated landcover raster
 def write_geotiff(dest_path, data, geo_transform, projection):
     #"""Create a GeoTIFF file with the given data."""
@@ -232,10 +300,9 @@ def write_geotiff(dest_path, data, geo_transform, projection):
     dataset=None
     
 # write and close the file for 2028 Business as usual
-write_geotiff(dest_path = join(out_path, 'data', 'lulc1'),        classif_map, transform, proj)
-write_geotiff(dest_path = join(out_path, 'data', 'uncertainty1'), uncert_map,  transform, proj)
-
-
+write_geotiff(dest_path = join(out_path, 'data', 'lulc_1.tif'),        classif_map, transform, proj)
+write_geotiff(dest_path = join(out_path, 'data', 'uncertainty_1.tif'), uncert_map,  transform, proj)
+'''
 
 # recrd the experiment time
 t1 = time.time()
@@ -252,58 +319,6 @@ pickle_save(name, experiment_time)
 
 
 print('Training time:', round((t1-t0)/60,2), 'mins')
-
-
-
-
-
-
-bands_list2 = []
-
-labels = rasterio.open(join(buffered_cos_path, 'buffer_1_cos2015.tif'))
-bands_list2.append(labels.read(1))
-
-rows, cols =  labels.read(1).shape
-
-x = np.arange(0, cols, 1)
-y = np.arange(0, rows, 1)
-xx,yy = np.meshgrid(x,y,)
-
-bands_list2.append(xx)
-bands_list2.append(yy)
-
-data_stack2 = np.dstack(bands_list2)
-
-_, _, n_bands = data_stack2.shape
-# number of pixels on one band
-n_pixels = rows*cols
-# stack bands as columns each (2D)
-flat_pixels2 = data_stack2.reshape((n_pixels, n_bands))
-del data_stack2
-
-# drop Nones using the rasterised COS
-index = np.argwhere(flat_pixels2[:,0]==-1)
-all_stack = np.delete(flat_pixels2, index, axis = 0)
-#del flat_pixels
-
-columns=['classes','x','y']
-
-
-df_0 = pd.DataFrame(all_stack,columns=columns) # shorter list
-df_0 = df_0.set_index(['x','y'])
-
-df_2 = pd.DataFrame(flat_pixels2, columns=columns) # full raster's list
-df_2.classes = np.nan
-df_2 = df_2.set_index(['x','y'])
-
-df_2 = df_2.join(df_0, lsuffix = '_old', rsuffix = '_new', how='outer')
-
-
-
-#df_0.reset_index().pivot('x','y','classes')
-
-
-
 
 
 
