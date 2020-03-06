79370d2c288311ef3cd72f9b84e911f6d4720dda
armkhudinyan <armkhudinyan@gmail.com>
2020-03-06 11:13:13 +0000

    Fixed a bug in the code

--- dgt_data/scripts/large_patch_selection.py
+++ dgt_data/scripts/large_patch_selection.py
@@ -4,6 +4,7 @@ Created on Thu Mar  5 23:50:08 2020
 
 @author: arman
 """
+import os
 from os.path import join, dirname
 import pandas as pd
 import geopandas as gpd
@@ -11,12 +12,16 @@ import geopandas as gpd
 shp_path = r'C:\Users\arman\Desktop\ActiveLearning\Experiment\dgt_T29SND\delete'
 gdf_uncert = gpd.read_file(join(shp_path,'uncert_intersect.shp'))
 
-gdf_uncert.groupby("Nomenclatu").size()
-gdf_uncert.groupby("classes").size()
+# define the amount of patches to be selected per class
+patch_num = 10
 
-classes = gdf_uncert.classes.unique().tolist()
+#==============================
+# Make dataframe containing class names and class IDs
+#==============================
+print('class sizes' ,gdf_uncert.groupby("Nomenclatu").size())
+#gdf_uncert.groupby("classes").size()
 
-class_id = gdf_uncert.classes.unique()
+class_id = sorted(gdf_uncert.classes.unique().tolist()) # should be sorted to match with class_size order
 class_size = gdf_uncert.groupby('classes').size()
 class_size_dict = dict(class_size)
 
@@ -26,23 +31,25 @@ to_list = [class_size_dict[keys] for keys in class_size_dict]
 sample_size = []
 
 for i in to_list:
-    if i >10:
-        sample_size.append(10)
+    if i > patch_num:
+        sample_size.append(patch_num)
     else:
         sample_size.append(i)
 
-#df_sample_size = pd.Series(sample_size)
+df = pd.DataFrame({'classes':class_id, 'size':sample_size})
 
-df = pd.DataFrame({'classes':classes, 'size':sample_size})
-
+#==============================
+# large patches selection
+#==============================
 # sample data by given sample size
 largest_patches = gdf_uncert.groupby('classes')\
                .apply(lambda x: x.sort_values(by='Area', ascending=False)\
                .iloc[:df.loc[df['classes']== x['classes'].iloc[0], 'size'].iloc[0],:])\
                .reset_index(drop=True)
     
-    # save the shapefile
-largest_patches.to_file('uncert_10_pathches.shp')
+# specify the directory to save save the shapefile
+os.chdir(r'C:\Users\arman\Desktop\ActiveLearning\Experiment\dgt_T29SND\delete')
+largest_patches.to_file('uncert_10_pathches2.shp')
 
 
 
